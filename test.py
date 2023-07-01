4caaedae5afc8e494a99dd79844cc851885312b6
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-07-01 11:22:02 +0200

    just for test

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 
 
 def my_func(*args):
-    return min(args)
+    return min(args, key=max)
 
 if __name__ == '__main__':
-    sol = my_func('1, 2, 3, 4 , 5', 'almafa', "Kati")
+    sol = my_func('1, 2, 3, 4 , 5', 'almafa', "Kati", "megszentségteleníthetetlenséges")
     print(sol)
